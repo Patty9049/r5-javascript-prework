92ceafb08cec45609bf593de1ec7c613790bddbb
patrycja <gumis9049@gmail.com>
2020-07-19 16:23:03 +0200

    Bug repair

--- js/script.js
+++ js/script.js
@@ -7,11 +7,10 @@ let computerMove = 'nieznany ruch';
 if(randomNumber == 1){
   computerMove = 'kamień';
 } else if (randomNumber == 2) {
-printMessage('papier');
+  computerMove = 'papier';
 } else if (randomNumber == 3) {
-printMessage('nożyce');
+  computerMove = 'nożyce';
 }
-
 printMessage('Mój ruch to: ' + computerMove);
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
@@ -22,16 +21,16 @@ let playerMove = 'nieznany ruch';
 
 if(playerInput == '1'){
   playerMove = 'kamień';
-} else if (playerInput == '2') {
-  playerMove = ='papier';
-} else if (playerInput == '3') {
-  playerMove = ='nożyce';
+} else if(playerInput == '2'){
+  playerMove = 'papier';
+} else if(playerInput == '3'){
+  playerMove = 'nożyce';
 }
 printMessage('Twój ruch to: ' + playerMove);
 
 if( computerMove == 'kamień' && playerMove == 'papier'){
   printMessage('Ty wygrywasz!');
-} else if ( computerMove == 'papier' && playerMove == 'nożyce'){
+}else if ( computerMove == 'papier' && playerMove == 'nożyce'){
   printMessage('Ty wygrywasz!');
 } else if ( computerMove == 'nożyce' && playerMove == 'kamień'){
   printMessage('Ty wygrywasz!');
@@ -41,13 +40,6 @@ if( computerMove == 'kamień' && playerMove == 'papier'){
   printMessage('Remis. Zagraj jeszcze raz.');
 } else if ( computerMove == 'nożyce' && playerMove == 'nożyce'){
   printMessage('Remis. Zagraj jeszcze raz.');
-} else { 
+} else {
   printMessage('Przegrana. Spróbuj jeszcze raz:(')
 }
-
-
-
-
-
-
-
